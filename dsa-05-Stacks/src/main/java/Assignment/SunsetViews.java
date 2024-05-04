96f23f79a13182135f6132af2d2828e519c0daa0
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-05-04 23:13:56 +0300

    SunsetViews is solved

---
+++ dsa-05-Stacks/src/main/java/Assignment/SunsetViews.java
@@ -0,0 +1,72 @@
+package Assignment;
+
+import CustomStackImplementation.MyStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+
+public class SunsetViews {
+
+    public static void main(String[] args) {
+        int[] buildings = {3, 5, 4, 4, 3, 1, 3, 2};
+        String direction = "EAST";
+
+        System.out.println(sunsetViews(buildings, direction));
+
+        //Output [1, 3, 6, 7] // return indices of buildings!!!
+    }
+
+    public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+        MyStack<Integer> myStack = new MyStack<>();
+
+        for (int i = 0; i < buildings.length; i++) {
+            int value= buildings[i]; //3
+            int temp=0;
+            int value2;
+            for (int j = i+1; j < buildings.length; j++) {
+                    value2 = buildings[j];
+                if (value2>temp) temp=value2;
+            }
+            if (value>temp) myStack.push(i);
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        while (!myStack.isEmpty()) {
+            arrayList.add(myStack.pop());
+        }
+        return arrayList;
+    }
+
+    public class SunsetViewsOptimum {
+        public static void main(String[] args) {
+            int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
+            sunsetViews(buildings,"West").forEach(System.out::println);
+        }
+        public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+            Stack<Integer> stack=new Stack<>();// stack to hold building indicies that can see sunset
+            // this part is for East by default
+            int i=0;
+            int step=1;
+            // this part is for west
+            if(direction.equalsIgnoreCase("west")) {
+                i= buildings.length-1;
+                step=-1;
+            }
+            while(i>=0 && i< buildings.length) {
+
+                while(!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
+                    stack.pop();
+                }
+                stack.push(i);
+                i+=step;
+            }
+            // now I have a stack with the building indicies that can see sunset
+            if(direction.equalsIgnoreCase("west")) Collections.reverse(stack);
+
+            return new ArrayList<Integer>(stack);
+        }
+    }
+}
--- dsa-05-Stacks/src/main/java/ExprBalanceCheck.java
+++ dsa-05-Stacks/src/main/java/ExprBalanceCheck.java
@@ -6,6 +6,9 @@ public class ExprBalanceCheck {
         System.out.println(balanceCheck("(A*C) + ({V-M})"));//true
         System.out.println(balanceCheck("(A*C) + ({V-M}"));//false
 
+        System.out.println(balanceCheckMine("(A*C) + ({V-M})"));//true
+        System.out.println(balanceCheck("(A*C) + ({V-M}"));//false
+
     }
 
     public static boolean balanceCheck(String expr) {
@@ -16,7 +19,7 @@ public class ExprBalanceCheck {
             Character ch = expr.charAt(i);
             //filter non player chars
             if(ch!='('&&ch!='['&&ch!='{'&&ch!=')'&&ch!=']'&&ch!='}') continue;
-            // if it is a symbol tobe processed
+            // if it is a symbol to be processed
             //if opening symbol
             if (ch=='('|| ch=='['||ch=='{') {
                 myStack.push(ch);
@@ -46,37 +49,31 @@ public class ExprBalanceCheck {
 
     public static boolean balanceCheckMine(String expr) {
         //create a stack
-        MyStack<Character> myStack= new MyStack<>();
-        //iterate over expr
+        int o1=0,o2=0,o3=0, o11=0,o22=0,o33=0;
         for (int i = 0; i < expr.length(); i++) {
-            Character ch = expr.charAt(i);
-            //filter non player chars
-            if(ch!='('&&ch!='['&&ch!='{'&&ch!=')'&&ch!=']'&&ch!='}') continue;
-            // if it is a symbol tobe processed
-            //if opening symbol
-            if (ch=='('|| ch=='['||ch=='{') {
-                myStack.push(ch);
-                continue;
-            }
-            // at this point this is a closing symbol
-            // stack shouldn't be empty
-            if (myStack.isEmpty()) return false;
-            //I need to check if they match or not
+            Character ch= expr.charAt(i);
             switch (ch) {
+                case '(':
+                    o1++;
+                    break;
                 case ')':
-                    if (myStack.pop()!='(') return false;
+                    o11++;
+                    break;
+                case '[':
+                    o2++;
                     break;
                 case ']':
-                    if (myStack.pop()!='[') return false;
+                    o22++;
+                    break;
+                case '{':
+                    o3++;
                     break;
                 case '}':
-                    if (myStack.pop()!='{') return false;
+                    o33++;
                     break;
             }
-        } // end of for
-        //return if stack is empty
-
-
-        return myStack.isEmpty();
+        }
+        if (o1==o11&&o2==o22&&o3==o33) return true;
+        return false;
     }
 }

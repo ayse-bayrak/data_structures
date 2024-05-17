7e243548a01e95c668ec1330e09880cdedb87366
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-17 11:04:56 +0300

    SunsetViews is solved

--- dsa-05-Stacks/src/main/java/AlgorithmProblemWithStack/Assignment/SunsetViews.java
+++ dsa-05-Stacks/src/main/java/AlgorithmProblemWithStack/Assignment/SunsetViews.java
@@ -8,13 +8,9 @@ import java.util.Collections;
 import java.util.Stack;
 
 public class SunsetViews {
-
     public static void main(String[] args) {
-        int[] buildings = {3, 5, 4, 4, 3, 1, 3, 2};
-        String direction = "EAST";
-
-        System.out.println(sunsetViews(buildings, direction));
-
+        int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
+        sunsetViews(buildings,"East").forEach(System.out::println);
         //Output [1, 3, 6, 7] // return indices of buildings!!!
     }
 
@@ -40,33 +36,4 @@ public class SunsetViews {
         return arrayList;
     }
 
-    public class SunsetViewsOptimum {
-        public static void main(String[] args) {
-            int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
-            sunsetViews(buildings,"West").forEach(System.out::println);
-        }
-        public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
-            Stack<Integer> stack=new Stack<>();// stack to hold building indicies that can see sunset
-            // this part is for East by default
-            int i=0;
-            int step=1;
-            // this part is for west
-            if(direction.equalsIgnoreCase("west")) {
-                i= buildings.length-1;
-                step=-1;
-            }
-            while(i>=0 && i< buildings.length) {
-
-                while(!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
-                    stack.pop();
-                }
-                stack.push(i);
-                i+=step;
-            }
-            // now I have a stack with the building indicies that can see sunset
-            if(direction.equalsIgnoreCase("west")) Collections.reverse(stack);
-
-            return new ArrayList<Integer>(stack);
-        }
-    }
 }

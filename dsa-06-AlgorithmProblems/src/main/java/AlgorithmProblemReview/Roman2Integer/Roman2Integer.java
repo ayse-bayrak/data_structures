83fa68f1b530af93f5465431329d2d22e94d2c2f
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-19 20:05:42 +0300

    Roman2Integer is solved

---
+++ dsa-06-AlgorithmProblems/src/main/java/AlgorithmProblemReview/Roman2Integer/Roman2Integer.java
@@ -0,0 +1,15 @@
+package AlgorithmProblemReview.Roman2Integer;
+
+public class Roman2Integer {
+    public static void main(String[] args) {
+        System.out.println(romanToInteger("CMXCIV"));
+    }
+
+    static int romanToInteger(String s) {
+        // ("M", 1000); ("D", 500);("C", 100);("L", 50);("X", 10);("V", 5);("I", 1);
+        // Create Map for Roman Numerals <String, Integer>
+        // create variables for sum, index
+        // while loop until index reaches end of string
+        return 0;
+    }
+}

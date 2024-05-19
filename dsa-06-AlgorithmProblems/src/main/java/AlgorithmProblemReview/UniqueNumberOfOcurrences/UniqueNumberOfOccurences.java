7a2dc273566012fef7e4ac83b9102d496beb14da
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-19 20:05:49 +0300

    UniqueNumberOfOccurences is solved

---
+++ dsa-06-AlgorithmProblems/src/main/java/AlgorithmProblemReview/UniqueNumberOfOcurrences/UniqueNumberOfOccurences.java
@@ -0,0 +1,32 @@
+package AlgorithmProblemReview.UniqueNumberOfOcurrences;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurences {
+        public static void main(String[] args) {
+            int [] myArray=new int[]{1,2,2,1,1,3};
+            //int [] myArray=new int[]{1,2};
+            System.out.println(uniqueOccurrences(myArray));
+        }
+        public static boolean uniqueOccurrences(int[] arr){
+            // create a hashmap (Integer,Integer) (Elements, Freq.)
+            Map<Integer, Integer> freq = new HashMap<>();
+            // count number of occurences and put them in map
+            //count frequencies pattern, keep in mind
+            for (int i : arr) {
+                freq.put(i, freq.getOrDefault(i, 0)+1);
+            }
+
+            // put the frequencies in a set find if there are any duplicated values
+
+            Set<Integer> freqSet= new HashSet<>();
+            for (Integer value : freq.values()) {
+                if(!freqSet.add(value)) return false;
+            }
+            // There are no duplicates , frequencies are unique..Then return true
+            return true;
+        }
+}

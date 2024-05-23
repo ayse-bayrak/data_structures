60924ed776da17527cd41e938872465835555226
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-23 21:00:44 +0300

    ReverseFirstKElementsQueueProblem is solved with stack and queue

--- dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
+++ dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
@@ -1,32 +1,58 @@
-import java.util.Stack;
+import java.util.*;
 
 public class ReverseFirstKElementsQueueProblem {
     public static void main(String[] args) {
-       MyQueue<Integer> queue=new MyQueue<>();
-        for (int i = 1; i < 6; i++) {
-            queue.enqueue(i);
-        }
-        System.out.println("Initial queue: ");
-        queue.printQueue();
-        System.out.println();
-        System.out.println("After reverse");
-        reverseFirstK(queue,3).printQueue();
+        Queue<Integer> deque = new LinkedList<>();
+        deque.add(10);
+        deque.add(20);
+        deque.add(30);
+        deque.add(40);
+        deque.add(50);
+        int k=4;
+        System.out.println(reverseK(deque,k));
+
+        //-------------------
+//        MyQueue<Integer> queue = new MyQueue<>();
+//        queue.enqueue(10);
+//        queue.enqueue(20);
+//        queue.enqueue(30);
+//        queue.enqueue(40);
+//        queue.enqueue(50);
+//
+//        reverseFirstK(queue, k);
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+
     }
-    static MyQueue<Integer> reverseFirstK(MyQueue<Integer> queue, int k){
-        //create a stack
-        Stack<Integer> stack=new Stack<>();
-        // push first K elements into stack
-        for (int i = 0; i < k; i++) {
-            stack.push(queue.dequeue());
-        }
-        // enqueue elements back into the queue
-        while(!stack.isEmpty()){
-            queue.enqueue(stack.pop());
-        }
-        // dequeue and enqueue size of queue -k times
-        for (int i = 0; i < queue.size-k ; i++) {
-            queue.enqueue(queue.dequeue());
-        }
-     return queue;
-    }
+
+   public static Queue<Integer> reverseK(Queue<Integer> deque, int k) {
+      // Deque<Integer> stack = new ArrayDeque<>();
+       int size = deque.size();
+       Stack<Integer> stack = new Stack<>();
+       for (int i = 0; i < k; i++) {
+           stack.push(deque.remove());
+       }
+       Integer d = deque.remove();
+
+       for (int i = 0; i < k; i++) {
+           deque.add(stack.pop());
+       }
+       deque.add(d);
+       return deque;
+   }
+
+   public static MyQueue<Integer> reverseFirstK(MyQueue<Integer> queue, int k) {
+        Stack<Integer> stack = new Stack<>();
+        int size = queue.size;
+       for (int i = 0; i < k ; i++) {
+           stack.push(queue.dequeue());
+       }
+       for (int i = 0; i < k; i++) {
+           queue.enqueue(stack.pop());
+       }
+       return queue;
+   }
+
 }

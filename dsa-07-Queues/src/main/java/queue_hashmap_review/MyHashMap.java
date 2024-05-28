08cae27e3afd947723f1162ab29581b2577d3c78
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-28 16:35:06 +0300

    LeetCodeProblem is started

---
+++ dsa-07-Queues/src/main/java/queue_hashmap_review/MyHashMap.java
@@ -0,0 +1,28 @@
+package queue_hashmap_review;
+
+public class MyHashMap {
+
+    public MyHashMap() {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+
+    public int get(int key) {
+return 1;
+    }
+
+    public void remove(int key) {
+
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */

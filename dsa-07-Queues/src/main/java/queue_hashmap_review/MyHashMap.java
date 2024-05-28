6e0cccdcae5bef336c476d0e03ee27642f29352e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-28 16:44:34 +0300

    MyHashMap and method are created

--- dsa-07-Queues/src/main/java/queue_hashmap_review/MyHashMap.java
+++ dsa-07-Queues/src/main/java/queue_hashmap_review/MyHashMap.java
@@ -1,21 +1,35 @@
 package queue_hashmap_review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyHashMap {
-
+List<BucketLinkedList> list;
+public int modNumber;
     public MyHashMap() {
-
+    this.modNumber=2003;
+    this.list= new ArrayList<BucketLinkedList>();
+        for (int i = 0; i < 2003; i++) {
+            this.list.add(new BucketLinkedList());
+        }
     }
 
     public void put(int key, int value) {
-
+        // calculate index from the key with modulus
+        int index = key% modNumber;
+        list.get(index).put(key, value);
     }
 
     public int get(int key) {
-return 1;
+        // calculate index with module go and visit the slots
+        // and then get the value associated with this in the link list.
+        int index = key %modNumber;
+    return list.get(index).get(key);
     }
 
     public void remove(int key) {
-
+        int index = key %modNumber;
+        list.get(index).remove(key);
     }
 }
 

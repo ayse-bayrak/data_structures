8ef257f9448102f6dd9a33dfb3606785dfae128e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-05-28 16:35:04 +0300

    LeetCodeProblem is started

---
+++ dsa-07-Queues/src/main/java/queue_hashmap_review/KeyValue.java
@@ -0,0 +1,11 @@
+package queue_hashmap_review;
+
+public class KeyValue <U, V>{
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}

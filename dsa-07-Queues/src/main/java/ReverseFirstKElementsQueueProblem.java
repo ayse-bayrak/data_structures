cadc4764e31077e4bba017edd3542ce112b1a313
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-05 19:12:00 +0300

    ReverseFirstKElementsQueueProblem is reviewed

--- dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
+++ dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
@@ -2,14 +2,26 @@ import java.util.*;
 
 public class ReverseFirstKElementsQueueProblem {
     public static void main(String[] args) {
-        Queue<Integer> deque = new LinkedList<>();
-        deque.add(10);
-        deque.add(20);
-        deque.add(30);
-        deque.add(40);
-        deque.add(50);
-        int k=4;
-        System.out.println(reverseK(deque,k));
+        MyQueue<Integer> queue = new MyQueue<>();
+        for (int i = 0; i < 6; i++) {
+            queue.enqueue(i);
+        }
+        System.out.println("Before reverse first K element");
+        queue.printQueue();
+        System.out.println();
+        System.out.println("After reverse first K element");
+        reverseFirstK(queue, 3).printQueue();
+
+
+        //------------------------
+//        Queue<Integer> deque = new LinkedList<>();
+//        deque.add(10);
+//        deque.add(20);
+//        deque.add(30);
+//        deque.add(40);
+//        deque.add(50);
+//        int k=4;
+//        System.out.println(reverseK(deque,k));
 
         //-------------------
 //        MyQueue<Integer> queue = new MyQueue<>();
@@ -44,13 +56,20 @@ public class ReverseFirstKElementsQueueProblem {
    }
 
    public static MyQueue<Integer> reverseFirstK(MyQueue<Integer> queue, int k) {
+        //create a stack
         Stack<Integer> stack = new Stack<>();
         int size = queue.size;
+        // push first K elements into stack
        for (int i = 0; i < k ; i++) {
            stack.push(queue.dequeue());
        }
-       for (int i = 0; i < k; i++) {
+       //enqueue elements back into the queue
+       while (!stack.isEmpty()) {
            queue.enqueue(stack.pop());
+       }
+      // dequeue and enqueue size of queue n-k times
+       for (int i = 0; i < queue.size-k; i++) {
+           queue.enqueue(queue.dequeue());
        }
        return queue;
    }

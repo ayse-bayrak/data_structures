a91052c8d3527fd8e6afe8071906a0381953b25c
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-06 16:50:51 +0300

    Traversing Trees

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
 
     TNode root;
@@ -31,6 +34,50 @@ public class MyTree {
             current=current.rightChild;
         }
     }
+    }
+    //Two mains types of Traversals:
+    //1-Breath First - Level Order
+    //2-Depth First
+    //Pre-Order Root L R
+    //In-Order L Root R
+    //Post-Order L R Root
+
+    //Root L R
+    void preOrderTraversal(TNode root) {
+        if(root==null) return; // termination-base condition
+        System.out.print(root.value+", "); // visit root
+        preOrderTraversal(root.leftChild); // visit left subtree
+        preOrderTraversal(root.rightChild); // visit right subtree
+    }
+
+    //L Root R
+    void inOrderTraversal(TNode root) {
+        if(root==null) return; // termination-base condition
+        // always remind-->recursive algorithm is to define the termination or base condition that will stop the algorithm
+        inOrderTraversal(root.leftChild); // visit left subtree
+        System.out.print(root.value+", "); // visit root
+        inOrderTraversal(root.rightChild); // visit right subtree
+    }
+
+    // L R Root --last element is gonna be  Root
+    void postOrderTraversal(TNode root) {
+        if(root==null) return; // termination-base condition
+        // always remind-->recursive algorithm is to define the termination or base condition that will stop the algorithm
+        postOrderTraversal(root.leftChild); // visit left subtree
+        inOrderTraversal(root.rightChild); // visit right subtree
+        System.out.print(root.value+", "); // visit root
+    }
+//this part was recursive
+    void levelOrderTraversal(TNode root) { // this time it will not be recursive
+        if(root==null) return;
+        Queue<TNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) { // Q not empty do
+            TNode toVisit = queue.poll();
+            System.out.print(toVisit.value+", ");
+            if (toVisit.leftChild!=null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild!=null) queue.add(toVisit.rightChild);
+        }
 
     }
 }
--- dsa-08-Trees/src/main/java/TreeApp.java
+++ dsa-08-Trees/src/main/java/TreeApp.java
@@ -6,5 +6,19 @@ public class TreeApp {
             tree.insert(numbers[i]);
         }
         VisualizeTree.printTree(tree.root, null, true);
+        System.out.println("Pre-Order Traversal of the Tree");
+        tree.preOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("In-Order Traversal of the Tree");
+        tree.inOrderTraversal(tree.root);//4, 5, 6, 8, 9, 10, 17, 20, 29, 42, 47
+        // in in order Traversal the output is ascending order
+        System.out.println();
+        System.out.println("Post-Order Traversal of the Tree");
+        tree.postOrderTraversal(tree.root);//5, 4, 8, 9, 6, 17, 20, 29, 42, 47, 10
+
+        System.out.println();
+        System.out.print("Level-Order Traversal of the Tree");
+        System.out.println();
+        tree.levelOrderTraversal(tree.root);
     }
 }

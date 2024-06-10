7e844801579212b00c065d9fe7f22880999a1808
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-10 07:29:45 +0300

    Review and test

---
+++ dsa-08-Trees/src/main/java/Trees_Review_1/Test.java
@@ -0,0 +1,20 @@
+package Trees_Review_1;
+
+public class Test {
+    public static void main(String[] args) {
+        LeetCodeTree tree=new LeetCodeTree();
+        tree.insert(10);
+        tree.insert(4);
+        tree.insert(12);
+        tree.insert(5);
+        VisualizeTree2.printTree(tree.root,null, false);
+        System.out.println(tree.searchBST(tree.root, 4).val);
+        System.out.println(tree.insertIntoBST(tree.root, 15).val);
+        VisualizeTree2.printTree(tree.root,null, false);
+        tree.preOrderTraversal(tree.root);
+        System.out.println();
+        tree.inOrderTraversal(tree.root);
+        System.out.println();
+        tree.postOrderTraversal(tree.root);
+    }
+}

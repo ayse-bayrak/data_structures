cb2c9547077f5e98bfda45e48b12c442fc6c6c67
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-23 19:42:36 +0300

    TNode is created

---
+++ dsa-08-Trees/src/main/java/Trees_Part2_Review/TNode.java
@@ -0,0 +1,12 @@
+package Trees_Part2_Review;
+
+public class TNode {
+    int value;
+    TNode leftChild;
+    TNode rightChild;
+
+    public TNode(int value) {
+        this.value = value;
+    }
+
+}

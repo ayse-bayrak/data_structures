5430e3712b2e2431d45f4a876cf18fc0cb0e83f2
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-06 09:32:49 +0300

    KthLargest is solved with heap structure

--- dsa-09-Heap/src/main/java/KthLargest.java
+++ dsa-09-Heap/src/main/java/KthLargest.java
@@ -3,6 +3,22 @@ public class KthLargest {
         int[] numbers={3,2,1,5,6,4};
         System.out.println(getKthLargest(numbers, 2));
     }
+
+    public static int getKthLargestF(int[] array, int k){
+        // 1.create a heap
+        MyHeap heap = new MyHeap(array.length);
+        // 2. Put array into a heap
+        for (int i = 0; i < array.length; i++) {
+            heap.insert(array[i]);
+        }
+        // 3. Remove root k-1 times
+        for (int i = 0; i <k-1 ; i++) {
+            heap.remove();
+        }
+        // 4. return root
+        return heap.peek();
+    }
+
     public static int getKthLargest(int[] array, int k){
         // create a heap
         MyHeap heap=new MyHeap(array.length);

3c299431ea3fe7f05d1b8bf5fc590c754885c002
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-11 14:21:37 +0300

    LRUCache is added and solved

---
+++ dsa-10-Search/src/main/java/LRUCacheProblemLeetcode/LRUCacheTestApp.java
@@ -0,0 +1,24 @@
+package LRUCacheProblemLeetcode;
+
+public class LRUCacheTestApp {
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        System.out.println(lRUCache.get(1));;    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        System.out.println(lRUCache.get(2));;    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        System.out.println(lRUCache.get(1));;    // return -1 (not found)
+        System.out.println(lRUCache.get(3));;    // return 3
+        System.out.println(lRUCache.get(4));;// return 4
+        lRUCache.printLRUCache();
+       /* LRUCache1 cache=new LRUCache1(3);
+        cache.put(1,1);
+        cache.put(2,2);
+        cache.put(3,3);
+        cache.put(4,4);
+        System.out.println(cache.get(1));
+        cache.printLRUCache();*/
+    }
+}

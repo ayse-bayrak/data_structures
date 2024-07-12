3c28634dcd4213fe9e7804ccda2d03939bbd3f97
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-12 12:07:18 +0300

    ExponentialSearch is added

--- dsa-10-Search/src/main/java/ExponentialSearch.java
+++ dsa-10-Search/src/main/java/ExponentialSearch.java
@@ -1,8 +1,9 @@
 public class ExponentialSearch {
     public static int exponentialSearch(int[] array, int data){
         int bound=1;
+        // to skip the blocks that can not contain data
         while (bound<array.length && array[bound]<data) bound*=2;
-
+        // found the suitable block make a binary search in that block
         int left=bound/2;
         int right=Math.min(bound, array.length-1);
         return BinarySearch.binarySearchRecursive(array, data, left,right);

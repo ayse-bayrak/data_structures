e3b294ae520e395207c7eefacfae9dabd8d862d9
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-14 23:26:07 +0300

    live review about sorting algorithm

--- dsa-11-Sorting/src/main/java/JD1QuickSort.java
+++ dsa-11-Sorting/src/main/java/JD1QuickSort.java
@@ -1,13 +1,15 @@
 public class JD1QuickSort {
     public static void qSort(int[] array, int start, int end){
-       if (start>=end) return; //termination, only one element or zero
-       int boundary=partition(array, start, end); // for pivot
+       if (start>=end) return; //termination of QSort, only one element or zero
+       int boundary=partition(array, start, end); // for pivot, boundary is last position of the pivot
+      // recursively call left part
        qSort(array, start, boundary-1);// call qSort for left part
+        // recursively call right part
        qSort(array,boundary+1, end);// call qSort for right part
     }
 
     public static int partition(int[] array, int start, int end){
-        int pivot=array[end];
+        int pivot=array[end]; // last element as pivot
         int boundary=start;
         for (int i = start; i <=end ; i++) {
             if(array[i]<=pivot) swap(array, i, boundary++);

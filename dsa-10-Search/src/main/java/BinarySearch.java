ebecaa497afa91c7b3924f4f37e2dc4b4edd5b68
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-07-14 14:26:20 +0300

    main and some changes

--- dsa-10-Search/src/main/java/BinarySearch.java
+++ dsa-10-Search/src/main/java/BinarySearch.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] numbers=new int[10];
@@ -6,6 +10,7 @@ public class BinarySearch {
         }
         System.out.println(binarySearchIterative(numbers, 6));
         System.out.println(binarySearchRecursive(numbers, 6));
+        System.out.println(binarySearchCollection(numbers, 6));
     }
     public static int binarySearchIterative(int[] array, int data){
         int left=0;
@@ -20,7 +25,7 @@ public class BinarySearch {
     }
 
     public static int binarySearchRecursive (int[] array, int data){
-        return binarySearchRecursive(array,data ,0,array.length-1 );
+        return binarySearchRecursive(array,data ,0,array.length-1 ); // basic ide encapsulate algorithm internal, so details we make private
     }
     public static int binarySearchRecursive (int[] array, int data, int left, int right){
         if (left>right) return-1;
@@ -30,5 +35,26 @@ public class BinarySearch {
         else return binarySearchRecursive(array, data,middle+1, right);
     }
 
+    public static int binarySearchCollection(int[] array, int data){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+
+        return Collections.binarySearch(list, data);
+    }
+
+    public static int search(int[] arr, int data) {
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+            if (data==arr[mid]) return mid;
+            if(data<=arr[mid]) right=mid-1;
+            else left = mid+1;
+        }
+        return -1;
+    }
 
 }
